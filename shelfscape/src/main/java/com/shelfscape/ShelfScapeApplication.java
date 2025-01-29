4e4168eef3b186140e14b334a218eddb5784fd49
timon <k.poloczek7@gmail.com>
2025-01-29 18:26:06 +0100

    initialize project

---
+++ shelfscape/src/main/java/com/shelfscape/ShelfScapeApplication.java
@@ -0,0 +1,13 @@
+package com.shelfscape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelfScapeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShelfScapeApplication.class, args);
+	}
+
+}
---
+++ shelfscape/src/test/java/com/shelfscape/ShelfScapeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shelfscape;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShelfScapeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
