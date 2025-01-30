6bff9936824f983d95966843aaddd4d039f37117
timon <k.poloczek7@gmail.com>
2025-01-30 20:35:26 +0100

    update Role and User services

--- shelfscape/src/main/java/com/shelfscape/service/UserService.java
+++ shelfscape/src/main/java/com/shelfscape/service/UserService.java
@@ -1,12 +1,13 @@
 package com.shelfscape.service;
 
+import com.shelfscape.model.Role;
 import com.shelfscape.model.User;
 import com.shelfscape.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.Collections;
 
 @Service
 public class UserService {
@@ -14,19 +15,27 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    public List<User> getAllUsers() {
-        return userRepository.findAll();
-    }
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
-    public Optional<User> getUserById(Long id) {
-        return userRepository.findById(id);
-    }
+    @Autowired
+    private RoleService roleService; // Usługa do zarządzania rolami
 
-    public User addUser(User user) {
+    public User registerUser(User user) {
+        // Sprawdzamy, czy użytkownik już istnieje
+        if (userRepository.findByEmail(user.getEmail()) != null) {
+            throw new RuntimeException("Email is already in use.");
+        }
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        Role userRole = roleService.getRoleByName("USER");
+        user.setRoles(Collections.singleton(userRole));
+
         return userRepository.save(user);
     }
 
-    public void deleteUser(Long id) {
-        userRepository.deleteById(id);
+    public String authenticate(User user) {
+        return "Login successful";
     }
 }
